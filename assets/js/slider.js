7fc56db6fae08efdce0a6617b0fb4f0212d30a9c
n0cti1ucent <doeshe@protonmail.com>
2023-04-09 15:34:43 +0900

    add files

---
+++ assets/js/slider.js
@@ -0,0 +1,41 @@
+$('.slider').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    pauseOnFocus: false,
+    slidesToShow: 3,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: true,//下部ドットナビゲーションの表示
+    responsive: [
+        {
+            breakpoint: 769,//モニターの横幅が769px以下の見せ方
+            settings: {
+                slidesToShow: 2,//スライドを画面に2枚見せる
+                slidesToScroll: 1,//1回のスクロールで2枚の写真を移動して見せる
+            }
+        },
+        {
+            breakpoint: 426,//モニターの横幅が426px以下の見せ方
+            settings: {
+                slidesToShow: 1,//スライドを画面に1枚見せる
+                slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+            }
+        }
+    ]
+});
+
+//index.html news sticker slider
+$('.news-sticker').slick({
+    autoplay: true,
+    infinite: true,
+    pauseOnFocus: false,
+    fade: false,
+    arrows: false,
+    speed: 1000,
+    vertical: true,
+    verticalSwiping: true,
+    centerMode: false,
+    slidesToShow: 1,
+    SlidesToScroll: 1
+});
